307ca0ee0aa71bb3fe4d040678b55d58097c9207
KenIjiwoye <alistluxury@gmail.com>
2021-08-14 16:10:45 +0000

    adding ui components. stuck on getting js files to work

---
+++ app/javascript/custom/index.js
@@ -0,0 +1,6 @@
+require("main");
+require("easing.min.js");
+require("jquery-migrate.min.js");
+require("owl.carousel.min.js");
+require("popper.min.js");
+require("scrollreveal.min.js");
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :amenities
   resources :properties
   devise_for :users
-  root 'public#index'
+  root 'properties#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
