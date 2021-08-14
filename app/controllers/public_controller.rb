352aefdb0880daaca4d5870546cb3075f1c38b16
KenIjiwoye <alistluxury@gmail.com>
2021-08-14 22:51:38 +0000

    change root url. continue adding ui elements.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,6 @@
 class PublicController < ApplicationController
   def index
     @properties = Property.all.order(created_at: :desc)
+    @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :amenities
   resources :properties
   devise_for :users
-  root 'properties#index'
+  root 'public#index'
   resources :public, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
