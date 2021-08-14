3a9f75d41ba39b991fedf1c756725d68a07564ac
KenIjiwoye <alistluxury@gmail.com>
2021-08-14 14:36:12 +0000

    model updates

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, dependent: :destroy
     has_many :amenities_properties
-    has_many :amenities, through: :amenities_properties
+    has_many :amenities, through: :amenities_properties, dependent: :destroy
 end
