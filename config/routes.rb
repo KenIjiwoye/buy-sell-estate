157c81e105d07c3620813b654f763d773f39c951
KenIjiwoye <alistluxury@gmail.com>
2021-08-15 14:28:03 +0000

    clean up ui a bit more.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   resources :amenities
   resources :properties
   devise_for :users
+  resources :users, only: [:index, :show]
   root 'public#index'
   resources :public, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
